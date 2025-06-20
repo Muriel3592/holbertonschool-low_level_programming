c0cbba34fcf9cd5a2391b8b8df87ff918c8a1ccf
muriel3592 <11522@holbertonstudents.com>
2025-06-20 08:42:47 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,22 @@
 # include <stdio.h>
 
+/**
+ * main -
+ *
+ * Return: always return 0
+ *
+ **/
+
+
 int main(void)
 {
 	char ch = 'a';
 
 	while (ch <= 'z')
-		putchar (ch++);
+	{
+		putchar(ch)
+		ch++
+	}
 
 	putchar ('\n');
 
